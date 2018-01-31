380cd574aea88eb45dbc2f6ca2042050a3872a7a
Mike McCready <michaeljmccready@gmail.com>
2018-01-30 18:23:05 -0800

    add comments

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,18 +1,26 @@
 const assert = require('assert')
 const ganache = require('ganache-cli')
 const Web3 = require('web3')
+// init provider from ganache network
 const provider = ganache.provider()
-
+// import compiled contract code
 const { interface, bytecode } = require('../compile')
 
+// create instance of web3 with ganache provider
 const web3 = new Web3(provider)
+const INIT_STRING = 'hello world'
 let accounts
 let inbox
-const INIT_STRING = 'hello world'
 
+// run before each test
 beforeEach(async () => {
+  // get list of accounts from ganache network
   accounts = await web3.eth.getAccounts()
 
+  // deploy with contract code
+  // set initial message with argument prop
+  // send transaction to create contract
+  // specify account and gas limit
   inbox = await new web3.eth.Contract(JSON.parse(interface))
   .deploy({ data: bytecode, arguments: [ INIT_STRING ] })
   .send({ from: accounts[0], gas: '1000000' })
@@ -21,15 +29,18 @@ beforeEach(async () => {
 })
 
 describe('Inbox', () => {
+  // verify that contract has an address
   it('deploys contract', () => {
     assert.ok(inbox.options.address)
   })
 
+  // verify that the initial message was set
   it('has a default message', async () => {
     const message = await inbox.methods.message().call()
     assert.equal(message, INIT_STRING)
   })
 
+  // test that the message can be updated using contract method
   it('can change the message', async () => {
     const NEW_MESSAGE = 'new world'
 
