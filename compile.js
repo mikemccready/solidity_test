8fe92c2f7fb4ba1354cccaa09fc6d96781605ce1
Mike McCready <michaeljmccready@gmail.com>
2018-01-30 18:22:16 -0800

    add comments

--- compile.js
+++ compile.js
@@ -2,7 +2,10 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
+// read file stream from contract
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
+// pass file stream to solidity compiler,
+// export 'Inbox' contract
 module.exports = solc.compile(source, 1).contracts[':Inbox'];
